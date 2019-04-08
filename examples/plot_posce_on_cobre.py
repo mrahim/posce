c4793428f62aaaf4d73346fe132e4f5b1feec280
Mehdi <rahim.mehdi@gmail.com>
2019-04-08 13:49:08 +0200

    optimize prior computing, clean code, docstring

--- examples/plot_posce_on_cobre.py
+++ examples/plot_posce_on_cobre.py
@@ -1,5 +1,7 @@
 from nilearn.datasets import fetch_atlas_msdl, fetch_cobre
 from nilearn.input_data import NiftiMapsMasker
+from nilearn.connectome import vec_to_sym_matrix
+from nilearn.plotting import plot_matrix
 from posce import PopulationShrunkCovariance
 
 # fetch atlas 
@@ -16,6 +18,10 @@ masker.fit()
 ts = [masker.transform(f) for f in cobre.func]
 
 # compute PoSCE on the same dataset
-posce = PopulationShrunkCovariance()
+posce = PopulationShrunkCovariance(shrinkage=1e-2)
 posce.fit(ts)
-sc = posce.transform(ts)
+connectivities = posce.transform(ts)
+
+# plot the first shrunk covariance
+cov = vec_to_sym_matrix(connectivities[0])
+plot_matrix(cov)
